d1f7f892aea4325e5a9ed57fb82a81afebdde0b4
Florent Le Gall <florent.legall@gmail.com>
2012-01-11 22:23:24 +0100

    Cosmetic change

--- comique/src/lobstre/comique/Comique.java
+++ comique/src/lobstre/comique/Comique.java
@@ -5,7 +5,8 @@ import java.awt.GraphicsEnvironment;
 
 public class Comique {
     public static void main (final String[] args) {
-        final GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment ().getDefaultScreenDevice ();
+        final GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment ();
+        final GraphicsDevice gd = ge.getDefaultScreenDevice ();
         final int width = gd.getDisplayMode ().getWidth ();
         final int height = gd.getDisplayMode ().getHeight ();
         System.out.println (width);
