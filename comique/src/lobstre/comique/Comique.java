2cbf7aa2d22c7f9ef696b91e52e47139ead87cfe
Florent Le Gall <florent.legall@gmail.com>
2012-01-11 22:22:21 +0100

    Fixed code style

--- comique/src/lobstre/comique/Comique.java
+++ comique/src/lobstre/comique/Comique.java
@@ -4,12 +4,11 @@ import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 
 public class Comique {
-	public static void main(final String[] args) {
-		final GraphicsDevice gd = GraphicsEnvironment
-				.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-		final int width = gd.getDisplayMode().getWidth();
-		final int height = gd.getDisplayMode().getHeight();
-		System.out.println(width);
-		System.out.println(height);
-	}
+    public static void main (final String[] args) {
+        final GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment ().getDefaultScreenDevice ();
+        final int width = gd.getDisplayMode ().getWidth ();
+        final int height = gd.getDisplayMode ().getHeight ();
+        System.out.println (width);
+        System.out.println (height);
+    }
 }
