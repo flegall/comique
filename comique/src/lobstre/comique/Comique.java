6a460a26f38c58df1dadc767e676055077e27e4a
Florent Le Gall <florent.legall@gmail.com>
2012-01-11 22:41:29 +0100

    Added directory checks

--- comique/src/lobstre/comique/Comique.java
+++ comique/src/lobstre/comique/Comique.java
@@ -1,9 +1,28 @@
 package lobstre.comique;
 
+import java.io.File;
+
 import lobstre.comique.util.Helper;
 
 public class Comique {
     public static void main (final String[] args) {
+        if (args.length != 1) {
+            System.out.println ("Usage : Comique directory");
+            return;
+        }
+        
+        final String dirName = args [0];
+        final File file = new File (dirName);
+        
+        if (!file.exists ()) {
+            System.out.println ("No such directory : " + file.getAbsolutePath ());
+            return;
+        }
+        if (!file.isDirectory ()) {
+            System.out.println  (file.getAbsolutePath () + " is not a directory");
+            return;
+        }
+        
         final int[] result = Helper.getScreenResolution ();
         System.out.println (result [0]);
     }
