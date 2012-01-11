9e884957f2e7b87e0d6390c6b75a8d122db17207
Florent Le Gall <florent.legall@gmail.com>
2012-01-11 22:54:21 +0100

    Added loadFiles ()

--- comique/src/lobstre/comique/Comique.java
+++ comique/src/lobstre/comique/Comique.java
@@ -2,11 +2,7 @@ package lobstre.comique;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.imageio.ImageIO;
+import java.util.Map;
 
 import lobstre.comique.util.Helper;
 
@@ -18,27 +14,20 @@ public class Comique {
         }
         
         final String dirName = args [0];
-        final File file = new File (dirName);
+        final File directory = new File (dirName);
         
-        if (!file.exists ()) {
-            System.out.println ("No such directory : " + file.getAbsolutePath ());
+        if (!directory.exists ()) {
+            System.out.println ("No such directory : " + directory.getAbsolutePath ());
             return;
         }
-        if (!file.isDirectory ()) {
-            System.out.println  (file.getAbsolutePath () + " is not a directory");
+        if (!directory.isDirectory ()) {
+            System.out.println  (directory.getAbsolutePath () + " is not a directory");
             return;
         }
         
-        final List<BufferedImage> images = new ArrayList<BufferedImage> ();
-        final File[] files = file.listFiles ();
-        for (final File f : files) {
-            try {
-                images.add (ImageIO.read(f));
-            } catch (final IOException e) {
-                e.printStackTrace();
-                continue;
-            }
-        }
+        final Map<Integer, BufferedImage> images = Helper.loadFiles (directory);
+        
+        System.out.println (images);
         
         final int[] result = Helper.getScreenResolution ();
         System.out.println (result [0]);
--- comique/src/lobstre/comique/util/Helper.java
+++ comique/src/lobstre/comique/util/Helper.java
@@ -2,6 +2,13 @@ package lobstre.comique.util;
 
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+import javax.imageio.ImageIO;
 
 public class Helper {
 
@@ -18,6 +25,28 @@ public class Helper {
         result [0] = gd.getDisplayMode ().getWidth ();
         result [1] = gd.getDisplayMode ().getHeight ();
         return result;
+    }
+
+    /**
+     * Loads all files
+     * 
+     * @param directory a directory {@link File}
+     * @return a {@link Map} of {@link Integer} page key to {@link BufferedImage}
+     * images
+     */
+    public static Map<Integer, BufferedImage> loadFiles (final File directory) {
+        final Map<Integer, BufferedImage> images = new ConcurrentSkipListMap<Integer, BufferedImage> ();
+        final File[] files = directory.listFiles ();
+        for (int i = 0; i < files.length; i++) {
+            final File f = files [i];
+            try {
+                images.put (i, ImageIO.read (f));
+            } catch (final IOException e) {
+                e.printStackTrace ();
+                continue;
+            }
+        }
+        return images;
     }
 
 }
