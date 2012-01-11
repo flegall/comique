0396523b0da42ecb38ff2c826fa9ea4ffe1b30c5
Florent Le Gall <florent.legall@gmail.com>
2012-01-11 22:17:49 +0100

    Added screen resolution detection

--- comique/src/lobstre/comique/Comique.java
+++ comique/src/lobstre/comique/Comique.java
@@ -1,7 +1,15 @@
 package lobstre.comique;
 
-public class Comique {
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 
-	public static void main(String[] args) {
+public class Comique {
+	public static void main(final String[] args) {
+		final GraphicsDevice gd = GraphicsEnvironment
+				.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		final int width = gd.getDisplayMode().getWidth();
+		final int height = gd.getDisplayMode().getHeight();
+		System.out.println(width);
+		System.out.println(height);
 	}
 }
