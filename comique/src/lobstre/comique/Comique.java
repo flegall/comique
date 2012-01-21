01683e30b61eb19d201f736b9ab95f82275edc56
Florent Le Gall <florent.legall@gmail.com>
2012-01-21 17:04:11 +0100

    Added loading progress bar.

--- comique/src/lobstre/comique/Comique.java
+++ comique/src/lobstre/comique/Comique.java
@@ -1,8 +1,15 @@
 package lobstre.comique;
 
+import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.SwingUtilities;
 
 import lobstre.comique.util.Helper;
 import lobstre.comique.util.Helper.ProgressListener;
@@ -26,18 +33,59 @@ public class Comique {
             return;
         }
         
-        final int[] result = Helper.getScreenResolution ();
+        final int[] screenRes = Helper.getScreenResolution ();
+        final JProgressBar[] progressBar = new JProgressBar [1];
+        
+        try {
+            SwingUtilities.invokeAndWait (new Runnable () {
+                @Override
+                public void run () {
+                    final JFrame jf = new JFrame ("Comique");
+                    jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    
+                    jf.setSize (PROGRESS_BAR_WIDTH, PROGRESS_BAR_HEIGHT);
+                    jf.setLocation (screenRes[0] / 2  - PROGRESS_BAR_WIDTH / 2, 
+                            screenRes[1] / 2 - PROGRESS_BAR_HEIGHT / 2);
+                    jf.setResizable (false);
+                    
+                    final JPanel pane = new JPanel ();
+                    pane.setLayout(new FlowLayout());
+                    progressBar[0] = new JProgressBar(0, 1000);
+                    progressBar[0].setValue(0);
+                    progressBar[0].setStringPainted(true);
+                    pane.add(progressBar[0]);
+                    jf.setContentPane (pane);
+                    jf.setVisible (true);
+                }
+            });
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return;
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+            return;
+        }
+        
+
         final Map<Integer, BufferedImage> images = Helper.loadFiles (
                 directory, 
-                result [0], 
+                screenRes [0], 
                 new ProgressListener() {
                     @Override
-                    public void progressed (int processedPages, int totalPages) {
-                        System.out.println (processedPages + "/" + totalPages);
+                    public void progressed (final int processedPages, final int totalPages) {
+                        SwingUtilities.invokeLater (new Runnable () {
+                            @Override
+                            public void run () {
+                                progressBar[0].setValue (processedPages);
+                                progressBar[0].setMaximum (totalPages);
+                            }
+                        });
                     }
                 });
         
         System.out.println ("Done: " + images.size () + " images loaded!");
-        
     }
+    
+    public static final int PROGRESS_BAR_WIDTH = 200;
+    public static final int PROGRESS_BAR_HEIGHT = 60;
 }
