2009c612247a8688d840b852fd34c8607e6c911c
Florent Le Gall <florent.legall@gmail.com>
2012-01-22 15:48:13 +0100

    Refactored File Chooser

--- comique/src/lobstre/comique/Comique.java
+++ comique/src/lobstre/comique/Comique.java
@@ -1,61 +1,18 @@
 package lobstre.comique;
 
-import java.awt.BorderLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.Map;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.SynchronousQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
 
 import lobstre.comique.util.Helper;
-import lobstre.comique.util.filedrop.FileDrop;
 
 public class Comique {
     public static void main (final String[] args) {
         final int[] screenRes = Helper.getScreenResolution ();
         
-        final BlockingQueue<File> queue = new SynchronousQueue<File> ();
-
-        SwingUtilities.invokeLater (new Runnable () {
-            @Override
-            public void run () {
-                final JFrame frame = new JFrame ("Comique file chooser");
-                final JLabel text = new JLabel ("Drop here a file or directory you'd like to read...");
-                text.setHorizontalAlignment (SwingConstants.CENTER);
-                frame.getContentPane ().add (text, BorderLayout.CENTER);
-
-                final int width = 300;
-                final int height = 80;
-                frame.setSize (width, height);
-                frame.setLocation (screenRes [0] / 2 - width / 2, screenRes [1] / 2 - height / 2);
-                frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-                frame.setVisible (true);
-
-                new FileDrop (System.out, text, new FileDrop.Listener () {
-                    public void filesDropped (File[] files) {
-                        if (files.length > 0) {
-                            queue.offer (files[0]);
-                            frame.setVisible (false);
-                            frame.dispose ();
-                        }
-                    } 
-                }); 
-
-            }
-        });
-
-        final File droppedFile;
-        try {
-            droppedFile = queue.take ();
-        } catch (InterruptedException e) {
-            e.printStackTrace ();
-            return;
-        }
+        final FileChooserDialog fcd = new FileChooserDialog (screenRes);
+        fcd.show ();
+        final File droppedFile = fcd.getFile ();
         
         final File directory;
         if (droppedFile.isDirectory ()) {
