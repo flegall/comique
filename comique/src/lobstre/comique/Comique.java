669349c05af98a13088c147af638fadff0038a70
Florent Le Gall <florent.legall@gmail.com>
2012-01-22 14:12:50 +0100

    Refactored the progress dialog

--- comique/src/lobstre/comique/Comique.java
+++ comique/src/lobstre/comique/Comique.java
@@ -2,7 +2,6 @@ package lobstre.comique;
 
 import java.awt.AWTEvent;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Point;
@@ -18,14 +17,11 @@ import java.util.Map;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
 
 import lobstre.comique.util.Helper;
-import lobstre.comique.util.Helper.ProgressListener;
 
 public class Comique {
     public static void main (final String[] args) {
@@ -47,43 +43,13 @@ public class Comique {
         }
 
         final int[] screenRes = Helper.getScreenResolution ();
-        final JProgressBar[] progressBar = new JProgressBar[1];
-        final JFrame[] jFrame = new JFrame[1];
+        
+        final ProgressDialog pd = new ProgressDialog (screenRes);
+        pd.show ();
 
-        SwingUtilities.invokeLater (new Runnable () {
-            @Override
-            public void run () {
-                jFrame [0] = new JFrame ("Comique");
-                jFrame [0].setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-
-                jFrame [0].setSize (PROGRESS_BAR_WIDTH, PROGRESS_BAR_HEIGHT);
-                jFrame [0].setLocation (screenRes [0] / 2 - PROGRESS_BAR_WIDTH / 2, screenRes [1] / 2 - PROGRESS_BAR_HEIGHT / 2);
-                jFrame [0].setResizable (false);
-
-                final JPanel pane = new JPanel ();
-                pane.setLayout (new FlowLayout ());
-                progressBar [0] = new JProgressBar (0, 1000);
-                progressBar [0].setValue (0);
-                progressBar [0].setStringPainted (true);
-                pane.add (progressBar [0]);
-                jFrame [0].setContentPane (pane);
-                jFrame [0].setVisible (true);
-            }
-        });
-
-        final Map<Integer, BufferedImage> images = Helper.loadFiles (directory, screenRes [0], new ProgressListener () {
-            @Override
-            public void progressed (final int processedPages, final int totalPages) {
-                SwingUtilities.invokeLater (new Runnable () {
-                    @Override
-                    public void run () {
-                        progressBar [0].setValue (processedPages);
-                        progressBar [0].setMaximum (totalPages);
-                    }
-                });
-            }
-        });
-
+        final Map<Integer, BufferedImage> images = Helper.loadFiles (directory, screenRes [0], pd.getProgressListener ());
+        
+        pd.hide ();
         System.out.println ("Done: " + images.size () + " images loaded!");
 
         SwingUtilities.invokeLater (new Runnable () {
@@ -92,9 +58,6 @@ public class Comique {
             
             @Override
             public void run () {
-                jFrame [0].setVisible (false);
-                jFrame [0].dispose ();
-
                 final JFrame jf = new JFrame ("Comique");
                 jf.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
                 jf.setUndecorated (true);
@@ -207,7 +170,4 @@ public class Comique {
             }
         });
     }
-
-    private static final int PROGRESS_BAR_WIDTH = 200;
-    private static final int PROGRESS_BAR_HEIGHT = 60;
 }
