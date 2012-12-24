2e978476d3fa5721c55a6a34adb8addbb461acad
Florent Le Gall <florent.legall@gmail.com>
2012-12-24 16:40:42 +0100

    Added JVM fork

---
+++ comique/src/lobstre/comique/Comique.java
@@ -0,0 +1,40 @@
+package lobstre.comique;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Comique {
+    public static void main (final String[] args) {
+        final String javaHome = System.getProperty ("java.home");
+        final File javaDir = new File (javaHome);
+        final File javaBinDir = new File (javaDir, "bin");
+        
+        final String classPath = System.getProperty ("java.class.path");
+        final String libraryPath = System.getProperty ("java.library.path");
+        
+        final String[] commands = new String [] {
+            javaBinDir + File.separator + "java",
+            "-cp",
+            classPath,
+            "-Djava.library.path="+libraryPath,
+            ComiqueInternal.class.getName (),
+            "-Xmx8192M"
+        };
+
+        try {
+            final Process process = Runtime.getRuntime ().exec (commands);
+            int read;
+            while (-1 != (read = process.getErrorStream ().read ())) {
+                System.err.write (read);
+            }
+            while (-1 != (read = process.getInputStream ().read ())) {
+                System.out.write (read);
+            }
+     
+            int exitValue = process.exitValue ();
+            System.out.println ("Exit Value : " + exitValue);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
